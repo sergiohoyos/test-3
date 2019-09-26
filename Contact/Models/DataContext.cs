e1bc1de555bf01fbfa6c158ddc9c16d46e55166e
Sergio hoyos <yeyohoyos3@gmail.com>
2019-09-26 18:46:22 -0400

    paso 6-2.0

--- Contact/Models/DataContext.cs
+++ Contact/Models/DataContext.cs
@@ -5,5 +5,7 @@ namespace Contact.Models
     using System.Data.Entity;
     public class DataContext:DbContext
     {
+        public DataContext() : base("DefaultConnection")
+        { }
     }
 }

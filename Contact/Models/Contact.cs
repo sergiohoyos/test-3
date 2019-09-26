6b97fe75bec81f17ebe605b6ad9adef1feaf6b1b
Sergio hoyos <yeyohoyos3@gmail.com>
2019-09-26 18:01:07 -0400

    paso1-2-3-4/2

---
+++ Contact/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+//ssss
+namespace Contact.Models
+{
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook,
+        instagram
+    }
+
+    public class Contact
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public TypeContact type { get; set; }
+        [Required]
+        public string ContactValue { get; set; }
+    }
+}
